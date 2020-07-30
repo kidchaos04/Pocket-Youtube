a344cbe3e31c4e28d68873f25defefb49f9ddf8c
kidchaos04 <60508772+kidchaos04@users.noreply.github.com>
2020-07-30 10:39:35 +0530

    Update fun.js

--- fun.js
+++ fun.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
 			data.items.forEach(item=>{
 				video=`
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<iframe width="100%" height="550" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 				`
 
 				$("#videos").append(video);
